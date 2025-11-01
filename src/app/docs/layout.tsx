58755a9ddc8c1be2bc434c6a3d1fad9718a5554d
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-11-01 14:39:59 +0545

    chore: Import older documentation

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -3,11 +3,41 @@ import { baseOptions } from "@/lib/layout.shared";
 import { source } from "@/lib/source/docs";
 
 export default function Layout({ children }: LayoutProps<"/docs">) {
+  const base = baseOptions();
   return (
     <DocsLayout
-      {...baseOptions()}
+      {...base}
       nav={{
+        ...base.nav,
         title: "PixelOS Docs",
+      }}
+      sidebar={{
+        tabs: {
+          transform(option, node) {
+            const meta = source.getNodeMeta(node);
+            if (!(meta && node.icon)) {
+              return option;
+            }
+
+            const color = `var(--${meta.path.split("/")[0]}-color, var(--color-fd-foreground))`;
+
+            return {
+              ...option,
+              icon: (
+                <div
+                  className="size-full rounded-lg text-(--tab-color) max-md:border max-md:bg-(--tab-color)/10 max-md:p-1.5 [&_svg]:size-full"
+                  style={
+                    {
+                      "--tab-color": color,
+                    } as object
+                  }
+                >
+                  {node.icon}
+                </div>
+              ),
+            };
+          },
+        },
       }}
       tree={source.pageTree}
     >
