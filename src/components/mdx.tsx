de2099abb7c920503dedc77a572fc4c2afdc026e
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2026-04-12 21:15:47 +0545

    feat: Add image zoom

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -1,8 +1,10 @@
+import { ImageZoom } from "fumadocs-ui/components/image-zoom";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
 
 export const getMDXComponents = (components?: MDXComponents) => ({
   ...defaultMdxComponents,
+  img: (props: typeof ImageZoom) => <ImageZoom {...props} />,
   ...components,
 });
 
